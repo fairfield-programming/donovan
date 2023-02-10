d8a4fa75ccbe3c26fa6a6082230acf56221285b8
Soham Das <publicsoham.24@gmail.com>
2023-02-10 10:40:17 +0530

    Tailwind styles generation successful

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,6 +1,7 @@
 const { findRepoConfig, createBabelConfig, createNPMConfig, installDependencies } = require('../config/index.js');
 const { downloadFromGitUrl, cleanupAfterTemplate } = require('../template/download.js');
 const runTemplate = require('../generator/index.js');
+const { generateTailwind } = require('../styles/index.js');
 
 const fs = require('fs');
 const path = require('path');
@@ -12,26 +13,29 @@ function run({ args, flags }) {
     (async() => {
 
         try {
-    
+
             const config = await findRepoConfig();
             global = {...global, project: config };
 
             console.log("Downloading Template from Git Url...");
             await downloadFromGitUrl(global.project.template);
-            
-            console.log("Generating Pages from Template...");
-            await runTemplate();
+
+            console.log("Creating Tailwind Config...");
+            await generateTailwind();
+
+            // console.log("Generating Pages from Template...");
+            // await runTemplate();
 
             console.log("Cleaning Up Leftover Assets...");
             await cleanupAfterTemplate();
-    
+
         } catch (e) {
-    
+
             console.log(e);
             process.exit(1);
-    
+
         };
-    
+
     })()
 
 }
---
+++ src/styles/index.js
@@ -0,0 +1,63 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+//absolute path to public folder
+const publicPath = path.resolve(process.cwd(), './public');
+
+function installTailwind() {
+    if (!fs.existsSync(publicPath)) {
+        console.log("Creating public directory...");
+        fs.mkdirSync(publicPath);
+    }
+}
+
+function addConfig() {
+    if (!fs.existsSync(path.resolve(publicPath + '/tailwind.config.js'))) {
+        fs.copyFile(process.cwd() + '/src/styles/tailwind.config.js', path.resolve(publicPath + '/tailwind.config.js'), (err) => {
+            if (err) throw err;
+            console.log('manual config pasted to public');
+        });
+    }
+}
+
+function addStyleDirectives() {
+    if (!fs.existsSync(path.resolve(publicPath + '/styles.css'))) {
+        fs.copyFile(process.cwd() + '/src/styles/styles.css', path.resolve(publicPath + '/styles.css'), (err) => {
+            if (err) throw err;
+            console.log('manual styles directives pasted to public');
+        });
+    }
+}
+
+function generateOutputFile() {
+    if (!fs.existsSync(path.resolve(publicPath + '/output.css'))) {
+        try {
+            fs.writeFileSync(path.resolve(publicPath + '/output.css'), '');
+            console.log('output.css created');
+
+            //double quotes are needed for eliminating errors due to white space in path.
+            execSync('npx tailwindcss -i ' + `\"${path.join(publicPath, 'styles.css')}\"` + ' -o ' + `\"${path.join(publicPath, 'output.css')}\"`);
+
+            console.log('output.css generated: ' + `\"${path.join(publicPath, 'output.css')}\"`);
+        } catch (err) {
+            throw Error(err);
+        }
+    }
+}
+
+function generateTailwind() {
+
+    //function calls are wrapped in a promise to ensure that they are executed in order.
+    Promise.all([
+        installTailwind(),
+        addConfig(),
+        addStyleDirectives(),
+    ]).then(res => {
+        generateOutputFile();
+    })
+}
+
+module.exports = {
+    generateTailwind
+}
