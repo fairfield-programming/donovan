bb6693810c4383663897ee4f74572057abc8a163
Soham Das <publicsoham.24@gmail.com>
2023-04-07 11:15:33 +0530

    test commit for potential error fixes

--- src/generator/runPage.js
+++ src/generator/runPage.js
@@ -1,82 +1,81 @@
-const Babel = require('@babel/standalone')
-const BabelJSX = require('@babel/preset-react')
+const Babel = require("@babel/standalone");
+const BabelJSX = require("@babel/preset-react");
 
-const metadataToHead = require('../utility/metadataToHead.js');
+const metadataToHead = require("../utility/metadataToHead.js");
 
-const vm = require('node:vm');
-const fs = require('fs');
-const path = require('path');
+const vm = require("node:vm");
+const fs = require("fs");
+const path = require("path");
 
-function importCssFile(name) {
+// function importCssFile(name) {
 
-    const contents = fs.readFileSync(name, 'utf-8');
+//     const contents = fs.readFileSync(name, 'utf-8');
 
-    fs.writeFileSync(path.join(process.cwd(), 'public', `${'3fF7F52AaVf3'}.css`), contents);
-   
-}
+//     fs.writeFileSync(path.join(process.cwd(), 'public', `${'3fF7F52AaVf3'}.css`), contents);
 
-let contextObject = { 
-    module: {},
-    exports: {},
-    global: {},
-    console
+// }
+
+let contextObject = {
+  module: {},
+  exports: {},
+  global: {},
+  console,
 };
 
 function getPageModuleFromPath(currentPath) {
-
-    const contents = fs.readFileSync(currentPath, 'utf-8');
-    return getPageModule(contents, currentPath);
-
+  const contents = fs.readFileSync(currentPath, "utf-8");
+  return getPageModule(contents, currentPath);
 }
 
 function getPageModule(contents, currentPath) {
+  var output = Babel.transform(contents, {
+    presets: [
+      "env",
+      [BabelJSX, { runtime: "automatic", importSource: "donovan" }],
+    ],
+  }).code;
 
-    var output = Babel.transform(contents, { presets: ["env", [ BabelJSX, { runtime: "automatic", importSource: "donovan" } ]] }).code;
+  contextObject = {
+    ...contextObject,
+    project: global.project,
+    require: (name) => {
+      const currentModulePath = currentPath;
 
-    contextObject = {
-        ...contextObject,
-        project: global.project,
-        require: (name) => {
+      if (name == "donovan/jsx-runtime")
+        return require("../core/jsx-runtime.js");
+      if (name == "donovan") return require("../core/index.js");
+      if (!name.startsWith(".") && !name.startsWith("/")) return require(name);
 
-            const currentModulePath = currentPath
-    
-            if (name == 'donovan/jsx-runtime') return require('../core/jsx-runtime.js')
-            if (name == 'donovan') return require('../core/index.js')
-            if (!name.startsWith('.') && !name.startsWith('/')) return require(name)
-    
-            const extension = path.extname(name).slice(1)
-            
-            if (extension == 'jsx')
-                return getPageModuleFromPath(path.join(process.cwd(), 'template', name)).module.exports
-    
-            if (extension == 'css'){
-                console.log("Using CSS");
-                return null;
-            }
-                //return importCssFile(path.join(currentModulePath, '../', name))
-    
-            return null
-    
-        },
-    };
+      const extension = path.extname(name).slice(1);
 
-    vm.createContext(contextObject);
-    vm.runInContext(output, contextObject);
+      if (extension == "jsx")
+        return getPageModuleFromPath(path.join(process.cwd(), "template", name))
+          .module.exports;
 
-    return contextObject;
+      if (extension == "css") {
+        console.log("Using CSS");
+        return null;
+      }
+      //return importCssFile(path.join(currentModulePath, '../', name))
 
+      return null;
+    },
+  };
+
+  vm.createContext(contextObject);
+  vm.runInContext(output, contextObject);
+
+  return contextObject;
 }
 
 function getPageHtml(pageModule) {
+  const bodyFunction = pageModule?.module?.exports || (() => {});
+  const headFunction = pageModule?.module?.exports?.getHead || (() => {});
+  const metadataFunction = pageModule?.module?.exports?.metadata || (() => {});
 
-    const bodyFunction = (pageModule?.module?.exports) || (() => { });
-    const headFunction = (pageModule?.module?.exports?.getHead) || (() => { });
-    const metadataFunction = (pageModule?.module?.exports?.metadata) || (() => { });
+  const headData = headFunction() || metadataToHead(metadataFunction()) || "";
 
-    const headData = headFunction() || metadataToHead(metadataFunction()) || "";
-
-    return `<!doctype html><html><head>${headData}</head><body>${bodyFunction()}</body></html>`;
-
+  return `<!doctype html><html><head>${headData}</head><body>${bodyFunction()}</body></html>`;
 }
 
 module.exports.getPageModule = getPageModule;
